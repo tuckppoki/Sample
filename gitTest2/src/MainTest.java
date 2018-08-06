bc2f4bb400bb608051644432086090b04e7a2c56
tuckppoki <milktegi@gmail.com>
2018-08-06 13:04:18 +0900

    MainTest 추가

---
+++ gitTest2/src/MainTest.java
@@ -0,0 +1,15 @@
+
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("world");
+		System.out.println("hotfix");
+		
+		
+		
+	}
+
+}
